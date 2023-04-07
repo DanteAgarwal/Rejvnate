e52b8ee9462151eecff17048dcff16d17d4ff197
Akshat Goyal <46156730+DanteAgarwal@users.noreply.github.com>
2023-04-07 15:47:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 echo "build start"
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate
+echo "migration start"
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+echo "collect static start"
 python3.9 manage.py collectstatic --noinput --clear
 echo "build end"
