bbfe77986d5ffad34241cc2a55ce43acf2691d68
Akshat Goyal <46156730+DanteAgarwal@users.noreply.github.com>
2023-04-07 16:15:25 +0530

    Update settings.py

--- rejuvate_website/settings.py
+++ rejuvate_website/settings.py
@@ -80,14 +80,7 @@ WSGI_APPLICATION = 'rejuvate_website.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT"),
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
